79c0d89ee5e180ca3648ff06b5b9c8b81720ced3
TE96 <odtuntun@gmail.com>
2020-04-29 10:22:11 +0800

    Add Week_03 day2 exercise.

---
+++ Week_03/GenerateParentheses.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    List<String> result = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        generateParenthesis(0, 0, n, "");
+        return result;
+    }
+    public void generateParenthesis(int left, int right, int n, String s) {
+        if (left == n && right == n) {
+            result.add(s);
+            return;
+        }
+        if (left < n) {
+            generateParenthesis(left + 1, right, n, s + '(');
+        }
+        if (right < left) {
+            generateParenthesis(left, right + 1, n, s + ')');
+        }
+    }
+}
---
+++ Week_03/PowXN.java
@@ -0,0 +1,14 @@
+public class PowXN {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        if (n < 0) {
+            // 处理n = Integer.MIN_VALUE的边界情况
+            x = 1 / x;
+            return x * myPow(x, -n - 1);
+        }
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        }
+        return x * myPow(x, n - 1);
+    }
+}
